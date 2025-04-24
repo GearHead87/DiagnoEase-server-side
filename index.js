d03efaf5c4c54531fbb66818fad86602e279dc36
GearHead87 <hosanulislam87@gmail.com>
2025-04-25 01:10:16 +0600

    fix(cors): Add new domain to CORS origins

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions = {
-	origin: ['https://diagnoease-1e7b9.web.app', 'https://diagno-ease-client-side.vercel.app', "http://localhost:5173"],
+	origin: ['https://diagnoease-1e7b9.web.app', 'https://diagno-ease-client-side.vercel.app', "http://localhost:5173", "https://www.cozycabin.space"],
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 	credentials: true,
